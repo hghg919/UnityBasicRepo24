67f2bc6cdfe5e94af413bdb0b800370ababe2860
hghg919 <tgeq1023@naver.com>
2024-11-19 17:46:49 +0900

    11-19 수정 전

--- UnityBasicLearn2024/Assets/Scripts/PlayerMove.cs
+++ UnityBasicLearn2024/Assets/Scripts/PlayerMove.cs
@@ -4,25 +4,18 @@ using UnityEngine;
 
 namespace UnityBasic
 {
-    public class Test : MonoBehaviour
+    public class PlayerMove : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
         {
-
+        
         }
 
         // Update is called once per frame
         void Update()
         {
-
-        }
-
-
-        private void OnTriggerEnter(Collider other)
-        {
-
+        
         }
     }
-
 }
